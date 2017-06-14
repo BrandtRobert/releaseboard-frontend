bee0c4e3324269bec013c7307943bbc06b03a6e9
breutimann <breutimann@envysion.com>
2017-06-14 16:36:01 -0600

    Initializing

---
+++ src/App.vue
@@ -0,0 +1,60 @@
+<template>
+  <v-app>
+    <v-toolbar>
+      <v-toolbar-title v-text="title"></v-toolbar-title>
+    </v-toolbar>
+    <main>
+      <v-container fluid>
+        <v-layout column align-center>
+          <data-table></data-table>
+        </v-layout>
+      </v-container>
+    </main>
+    <v-navigation-drawer
+      temporary
+      :right="right"
+      v-model="rightDrawer"
+    >
+      <v-list>
+        <v-list-item>
+          <v-list-tile @click.native="right = !right">
+            <v-list-tile-action>
+              <v-icon light>compare_arrows</v-icon>
+            </v-list-tile-action>
+            <v-list-tile-title>Switch drawer (click me)</v-list-tile-title>
+          </v-list-tile>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+    <v-footer :fixed="fixed">
+      <span>&copy; 2017</span>
+    </v-footer>
+  </v-app>
+</template>
+
+<script>
+  import DataTable from './DataTable.vue'
+
+  export default {
+    name: app,
+    components: { DataTable },
+    data () {
+      return {
+        clipped: false,
+        drawer: true,
+        fixed: false,
+        items: [
+          { icon: 'bubble_chart', title: 'Inspire' }
+        ],
+        miniVariant: false,
+        right: true,
+        rightDrawer: false,
+        title: 'Envysion Engineering Team -- Releases and Versioning'
+      }
+    }
+  }
+</script>
+
+<style lang="stylus">
+  @import './stylus/main'
+</style>
---
+++ src/DataTable.vue
@@ -0,0 +1,71 @@
+<template>
+  <v-data-table
+    v-bind:headers="headers"
+    v-bind:items="items"
+    hide-actions
+    class="elevation-1"
+  >
+  <template slot="headers" scope="props">
+    <span v-tooltip:bottom="{ 'html': props.item.text }">
+      {{ props.item.text }}
+    </span>
+  </template>
+  <template slot="items" scope="props">
+    <td>
+      <v-text-field :value="props.item.package" v-model="props.item.package">
+      </v-text-field>
+    </td>
+    <td class="text-xs-right">
+      <v-text-field :value="props.item.release" v-model="props.item.release">
+      </v-text-field>
+    </td>
+    <td class="text-xs-right">
+      <v-text-field :value="props.item.version" v-model="props.item.version">
+      </v-text-field>
+    </td>
+    <td> 
+      <v-checkbox
+        hide-details
+        success
+        v-model="props.item.merged"
+      ></v-checkbox>
+    </td>
+  </template>
+  </v-data-table>
+</template>
+
+<script>
+export default {
+  data () {
+      return {
+        headers: [
+            { text: 'Package', value: 'package', left: true },
+            { text: 'Product Release', value: 'release', sortable: false, left: true },
+            { text: 'Version', value: 'version', sortable: false, left:true},
+            { text: 'Merged', value: 'merged', sortable: false, left:true}
+        ],
+        items: [
+          {
+            value: false,
+            package: 'Service',
+            release: '406',
+            version: '5.28.1',
+            merged: false
+          },
+          {
+            value: false,
+            package: 'Video Web',
+            release: '406',
+            version: '4.28.0',
+            merged: false
+          }
+        ]
+      }
+  }
+}
+</script>
+
+<style>
+</style>
+
+
